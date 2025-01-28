6409a207114378099d326a02c4d9698815559ad4
parksh <tmdghks0906@gmail.com>
2025-01-28 14:42:40 +0900

    test

---
+++ DockerConsoleApp/DockerConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string path = "/app/myfolder"; // Docker 컨테이너 내의 경로
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+            Console.WriteLine($"Directory created at {path}");
+        }
+        else
+        {
+            Console.WriteLine($"Directory already exists at {path}");
+        }
+    }
+}
